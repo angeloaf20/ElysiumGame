54bab6378811fd75602102b5394200a2d0dc6666
angeloaf20 <angelofoster01@gmail.com>
2024-09-23 03:00:52 -0400

    Fixing up input controller - calculating button hold duration

--- src/Input/InputController.ts
+++ src/Input/InputController.ts
@@ -1,23 +1,33 @@
+type Key = {
+    Code: string,
+    Duration: number
+};
+
 export class InputController {
-    public Keys: { [key: string]: boolean } = {
-        ArrowLeft: false,
-        ArrowRight: false,
-        ArrowUp: false,
-        ArrowDown: false,
-        KeyC: false,
-        KeyX: false,
+    public Keys: { [key: string]: number } = {
+        ArrowLeft: 0, // move left
+        ArrowRight: 0, // move rightc
+        ArrowUp: 0, // jump
+        ArrowDown: 0, // move down (from platform)
+        KeyC: 0, // light attack
+        KeyX: 0, // heavy attack
+        KeyZ: 0 // shield/block
     };
+    //private listenWindow: number; // listen for how long a key is pressed
 
     constructor() {
         addEventListener("keydown", (event: KeyboardEvent) => {
             if (event.code in this.Keys) {
-                this.Keys[event.code] = true;
+                this.Keys[event.code] = Date.now();
+                //this.Keys[event.code] = true;
             }
         });
 
         addEventListener("keyup", (event: KeyboardEvent) => {
             if (event.code in this.Keys) {
-                this.Keys[event.code] = false;
+                this.Keys[event.code] -= Date.now();
+                //this.Keys[event.code] = false;
+                console.log(this.Keys);
             }
         });
     }
